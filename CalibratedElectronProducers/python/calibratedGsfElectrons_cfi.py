2f87bb3897a304238c193dbdd794e61a32d4cce1
charlot <>
2012-06-12 09:45:03 +0000

    fix default value for isAOD in module config

--- CalibratedElectronProducers/python/calibratedGsfElectrons_cfi.py
+++ CalibratedElectronProducers/python/calibratedGsfElectrons_cfi.py
@@ -16,18 +16,13 @@ calibratedGsfElectrons = cms.EDProducer("CalibratedGsfElectronProducer",
     isMC = cms.bool(False),
 
     # set to True to read AOD format
-    isAOD = cms.bool(False),
+    isAOD = cms.bool(True),
     
     # set to True to get debugging printout   
     debug = cms.bool(False),
     
-    # input datasets
-    # Prompt means May10+Promptv4+Aug05+Promptv6 for 2011
-    # ReReco means Jul05+Aug05+Oct03 for 2011
-    # Jan16ReReco means Jan16 for 2011
-    # Summer11 means summer11 MC..
-    #inputDataset = cms.string("ReReco"),
-    inputDataset = cms.string("Prompt"),
+    # set to True to propagate MC extra smearing to the electron momentum error  
+    updateEnergyError = cms.bool(False),
     
 )
 
