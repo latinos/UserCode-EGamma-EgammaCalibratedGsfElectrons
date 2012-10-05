16c280ff61796413fe255e00dce99946a312ec14
mdalchen <>
2012-10-05 08:27:17 +0000

    Bagfix in corrections, dataset name update

--- CalibratedElectronProducers/python/calibratedPatElectrons_cfi.py
+++ CalibratedElectronProducers/python/calibratedPatElectrons_cfi.py
@@ -29,6 +29,9 @@ calibratedPatElectrons = cms.EDProducer("CalibratedPatElectronProducer",
     # ReReco means Jul05+Aug05+Oct03 for 2011
     # Jan16ReReco means Jan16 for 2011
     # Summer11 means summer11 MC..
+    # Summer12 means summer12 MC pre-ICHEP.
+    # Summer12_DR53X_HCP2012 means summer12 MC for HCP 2012.
+    # 2012Jul13ReReco
     #inputDataset = cms.string("ReReco"),
     inputDataset = cms.string("Prompt"),
     
